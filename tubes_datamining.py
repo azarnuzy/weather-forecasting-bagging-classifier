3c62cd7218687b2612db3f0c4fe0725cebecd7e9
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-01-03 02:52:57 +0700

    Change Decision Tree Classifier to  Bagging Classifier in python file

--- tubes_datamining.py
+++ tubes_datamining.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, render_template
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
@@ -10,62 +11,54 @@ app = Flask(__name__)
 # from app import prediction
 df = pd.read_csv("weatherHistory.csv")
 
-#drop data dalam kolom yang masih berisi nAn
+# Preprocessing
+# Drop NaN Value
 df = df.dropna()
-
-#drop data dalam kolom summary yang berisi kata 'and'
+# Drop record contains 'and' substring in target column (Summary)
 andIdx = df[df['Summary'].str.contains("and")].index
 df.drop(andIdx , inplace=True)
-
-# #melihat data unik dalam summary
-# df['Summary'].unique()
-
-# df.columns
-
-# Preprocessing
+# encoding
 label_encoder = preprocessing.LabelEncoder()
 df['Precip Type'] = label_encoder.fit_transform(df['Precip Type'])
 df['Summary'] = label_encoder.fit_transform(df['Summary'])
-
-# df['Precip Type'].isnull().sum()
+# fill NA with median
 med = df['Precip Type'].median()
 df['Precip Type'].fillna(med, inplace=True)
-
-# df['Summary'].unique()
 
 # Prepare Data Train & Data Test
 features = ['Precip Type', 'Temperature (C)', 'Apparent Temperature (C)', 'Humidity', 'Wind Speed (km/h)',
             'Wind Bearing (degrees)', 'Visibility (km)', 'Pressure (millibars)']
 target = 'Summary'
-# data training 85%
-# data testing 15%
+
+# data training 70%
+# data testing 30%
 x_train, x_test, y_train, y_test = train_test_split(df[features], df[target],
                                                     train_size=0.85, test_size=0.15, shuffle=False)
 # print("X_train: {}, Y_train: {}".format(len(x_train), len(x_test)))
 # print("X_train: {}, Y_train: {}".format(len(y_train), len(y_test)))
 
-# Train Model
+'''
+# Train Model Decision Tree
 model = DecisionTreeClassifier(max_depth=18)
 model.fit(x_train, y_train)
+'''
 
-# # Testing Model
-# print ("Training Accuracy: {}".format(model.score(x_train, y_train)))
-# predicted = model.predict(x_test)
-# print ("Testing Accuracy: {}".format(accuracy_score(y_test, predicted)))
+# Train Model Bagging Classifier
+dtc = DecisionTreeClassifier(criterion="entropy")
+bag_model = BaggingClassifier(base_estimator=dtc, n_estimators=100, bootstrap=True)
+bag_model = bag_model.fit(x_train, y_train)
 
-# # Measure Performance (Cross Validation)
-# print("Cross Validation Accuracy: \n")
-# cv_accuracy = cross_val_score(estimator=model, X=x_train, y=y_train, cv=10)
-# print("Accuracy using 10 folds: ")
-# print(cv_accuracy)
+'''
+# Accuracy Testing
+predicted = bag_model.predict(x_test)
+print(bag_model.score(x_test, y_test))
 
-# # Measure Performance (Confusion Matrix)
-# print("Mean accuracy: {}".format(cv_accuracy.mean()))
-# print("Standard Deviation: {}".format(cv_accuracy.std()))
+# Confusion Matrix
+print(confusion_matrix(y_test, predicted))
 
-# # Measure Performance
-# print('Precision, Recall and f-1 Scores for Decision Tree Classifier\n')
-# print(classification_report(y_test, predicted))
+# Measure Performance
+print(classification_report(y_test, predicted))
+'''
 
 # tampilan index (halaman awal)
 @app.route('/')
@@ -87,7 +80,7 @@ def prediction():
   visibility = float(request.form['visibility'])
   pressure = float(request.form['pressure'])
 
-  predicted = model.predict([[precipType, temperature, apparentTemp, humidity, windSpeed, windBearing, visibility, pressure]])
+  predicted = bag_model.predict([[precipType, temperature, apparentTemp, humidity, windSpeed, windBearing, visibility, pressure]])
   result = ""
 
   precipType = "Snow" if precipType == 1 else "Rain"
